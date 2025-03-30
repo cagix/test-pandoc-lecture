fb92de1911eee01bc403b1d0a3fce90a5325b393
Carsten Gips <cagix@hsbi.de>
2025-03-30 18:37:49 +0200

    add filter example: how to add in front of the document

--- .pandoc/filters/ghalerts.lua
+++ .pandoc/filters/ghalerts.lua
@@ -5,3 +5,11 @@ function Div(el)
         return pandoc.BlockQuote({pandoc.RawBlock("markdown", '[!NOTE]')} .. el.content)
     end
 end
+
+--- TODO TEST
+function Pandoc(doc)
+--    firstblock = pandoc.Para({pandoc.Str("FOO"), pandoc.HorizontalRule()})
+    firstblock = pandoc.Para(pandoc.Str("FOO"))
+    table.insert(doc.blocks, 1, firstblock)
+    return pandoc.Pandoc(doc.blocks, doc.meta)
+end
