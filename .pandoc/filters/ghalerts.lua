6f363f0b783ac56c08f638d42b959d5f23c3c84f
Carsten Gips <cagix@hsbi.de>
2025-03-31 17:51:47 +0200

    filter: add code to format outcomes, quizzes, challenges

--- .pandoc/filters/ghalerts.lua
+++ .pandoc/filters/ghalerts.lua
@@ -65,6 +65,49 @@ function Pandoc(doc)
 
     blocks:insert(pandoc.HorizontalRule())
 
+    -- 4. Outcomes, Quizzes, and Challenges
+    if doc.meta.outcomes or doc.meta.quizzes or doc.meta.challenges then
+        local quote = pandoc.List()
+
+        quote:insert(pandoc.RawBlock("markdown", '[!TIP]'))
+
+        if doc.meta.outcomes then
+            local bullets = pandoc.List()
+            for _, e in ipairs(doc.meta.outcomes) do
+                for k, v in pairs(e) do
+                    bullets:insert(pandoc.Str(k .. ": " .. pandoc.utils.stringify(v)))
+                end
+            end
+            quote:insert(pandoc.RawBlock("markdown", '<details>'))
+            quote:insert(pandoc.RawBlock("markdown", '<summary><strong>Lernziele</strong></summary>'))
+            quote:insert(pandoc.BulletList(bullets))
+            quote:insert(pandoc.RawBlock("markdown", '</details>'))
+        end
+
+        if doc.meta.quizzes then
+            local bullets = pandoc.List()
+            for _, v in ipairs(doc.meta.quizzes) do
+                local str_link = pandoc.utils.stringify(v.link)
+                bullets:insert(pandoc.Link(v.name or str_link, str_link))
+            end
+            quote:insert(pandoc.RawBlock("markdown", '<details>'))
+            quote:insert(pandoc.RawBlock("markdown", '<summary><strong>Quizzes</strong></summary>'))
+            quote:insert(pandoc.BulletList(bullets))
+            quote:insert(pandoc.RawBlock("markdown", '</details>'))
+        end
+
+        if doc.meta.challenges then
+            quote:insert(pandoc.RawBlock("markdown", '<details>'))
+            quote:insert(pandoc.RawBlock("markdown", '<summary><strong>Challenges</strong></summary>'))
+            quote:extend(doc.meta.challenges)
+            quote:insert(pandoc.RawBlock("markdown", '</details>'))
+        end
+
+        blocks:insert(pandoc.BlockQuote(quote))
+    end
+
+
+
 
     if doc.meta.refs then
         hblocks:insert(pandoc.Header(2, "Quellen"))
