72c833c4ddf6c1d3bf453f04b5b0cbe17292a7a9
Carsten Gips <cagix@hsbi.de>
2026-03-04 18:00:35 +0100

    crawl/book.md: remove linkification (for now)

--- crawl.lua
+++ crawl.lua
@@ -608,7 +608,7 @@ local function _emit_book_md (root)
                 Link = function(el)
                     local t = _is_local_link(el.target)
                     if t then
-                        warn("removing link for [" .. utils.stringify(el.content) .. "](" .. utils.stringify(el.target) .. ")")
+                        warn("removing internal link for [" .. utils.stringify(el.content) .. "](" .. utils.stringify(el.target) .. ")")
                         return el.content -- remove link (for now)
                     end
                 end
